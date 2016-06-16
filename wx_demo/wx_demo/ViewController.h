5994c4ba3b61df703cdd21afd4b7837f35c46bb8
李林 <ww1095@163.com>
2016-06-16 16:45:24 +0800

    init

---
+++ wx_demo/wx_demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  wx_demo
+//
+//  Created by 李林 on 16/6/16.
+//  Copyright © 2016年 李林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
